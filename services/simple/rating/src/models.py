adaab705ebbb122b821eb92296b3e4ad9f8e60f5
nneelie <eileenlee31@gmail.com>
2024-03-12 01:07:32 +0800

    routes

--- services/simple/rating/src/models.py
+++ services/simple/rating/src/models.py
@@ -1,18 +1,21 @@
 from db import db
+import uuid
 
 class ClinicRating(db.Model):
     __tablename__ = 'clinic_rating'
     
-    ratingID = db.Column(db.String(36), primary_key=True)
+    ratingID = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     clinicID = db.Column(db.String(36))
-    ratingGiven = db.Column(db.Float)
+    ratorID = db.Column(db.String(36))
+    ratingGiven = db.Column(db.Integer)
     timeStamp = db.Column(db.DateTime)
     ratingComment = db.Column(db.VARCHAR(255))
 
 
-    def __init__(self, ratingID, clinicID, ratingGiven, timeStamp, ratingComment):
+    def __init__(self, ratingID, clinicID, ratorID, ratingGiven, timeStamp, ratingComment):
         self.ratingID = ratingID
         self.clinicID = clinicID
+        self.ratorID = ratorID
         self.ratingGiven = ratingGiven
         self.timeStamp = timeStamp
         self.ratingComment = ratingComment
@@ -21,6 +24,7 @@ class ClinicRating(db.Model):
         return {
             "ratingID": self.ratingID,
             "clinicID": self.clinicID,
+            "ratorID": self.ratorID,
             "ratingGiven": self.ratingGiven,
             "timeStamp": self.timeStamp,
             "ratingComment": self.ratingComment,
@@ -29,13 +33,12 @@ class ClinicRating(db.Model):
 class DoctorRating(db.Model):
     __tablename__ = 'doctor_rating'
     
-    ratingID = db.Column(db.String(36), primary_key=True)
+    ratingID = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     clinicID = db.Column(db.String(36))
-    doctorID = db.column(db.string(36))
-    appointmentID = db.column(db.string(36))
-# not sure how to do foreign key
-    ratorID = db.Column(db.Integer, db.ForeignKey("??"))
-    ratingGiven = db.Column(db.Float)
+    doctorID = db.column(db.String(36))
+    appointmentID = db.column(db.String(36))
+    ratorID = db.Column(db.String(36))
+    ratingGiven = db.Column(db.Integer)
     timeStamp = db.Column(db.DateTime)
     ratingComment = db.Column(db.VARCHAR(255))
 

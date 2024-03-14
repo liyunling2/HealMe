c8024cbd41bb96c6d4451a8f1f7420e289511fa7
ChengRong <crkee.2022@scis.smu.edu.sg>
2024-03-14 13:22:18 +0800

    booking compose, setup, env

--- services/db/setup.sql
+++ services/db/setup.sql
@@ -17,3 +17,8 @@ CREATE DATABASE IF NOT EXISTS `rating`;
 
 CREATE USER IF NOT EXISTS 'rating'@'%' IDENTIFIED BY 'rating';
 GRANT ALL PRIVILEGES ON rating.* TO 'rating'@'%';
+
+CREATE DATABASE IF NOT EXISTS `booking`;
+
+CREATE USER IF NOT EXISTS 'booking'@'%' IDENTIFIED BY 'booking';
+GRANT ALL PRIVILEGES ON booking.* TO 'booking'@'%';

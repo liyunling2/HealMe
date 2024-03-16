e6c147d486c8593583f8891ef1b46000f1678570
liyunling2 <yunling.li.2022@smu.edu.sg>
2024-03-16 23:20:46 +0800

    ampq, log set up

---
+++ services/simple/log/src/amqp_setup.py
@@ -0,0 +1,64 @@
+import time
+import pika
+from os import environ
+
+hostname = environ.get('hostname') 
+port = environ.get('port')       
+exchangename = "log_fanout"
+exchangetype = "fanout"
+ 
+
+def create_connection(max_retries=12, retry_interval=5):
+    print('amqp_setup:create_connection')
+    
+    retries = 0
+    connection = None
+    
+    while retries < max_retries:
+        try:
+            print('amqp_setup: Trying connection')
+            
+            connection = pika.BlockingConnection(pika.ConnectionParameters
+                                (host=hostname, port=port,
+                                 heartbeat=3600, blocked_connection_timeout=3600)) 
+            print("amqp_setup: Connection established successfully")
+            break  
+
+        except pika.exceptions.AMQPConnectionError as e:
+            print(f"amqp_setup: Failed to connect: {e}")
+            retries += 1
+            print(f"amqp_setup: Retrying in {retry_interval} seconds...")
+            time.sleep(retry_interval)
+
+    if connection is None:
+        raise Exception("amqp_setup: Unable to establish a connection to RabbitMQ after multiple attempts.")
+
+    return connection
+
+def create_channel(connection):
+    print('amqp_setup:create_channel')
+    channel = connection.channel()
+    print('amqp_setup:create exchange')
+    channel.exchange_declare(exchange=exchangename, exchange_type=exchangetype, durable=True) 
+
+    return channel
+
+
+def create_queues(channel):
+    print('amqp_setup:create queues')
+    create_log_queue(channel)
+
+  
+def create_log_queue(channel):
+    print('amqp_setup:create_activity_log_queue')
+    log_queue_name = 'All_Logs'
+    channel.queue_declare(queue=log_queue_name, durable=True) 
+    channel.queue_bind(exchange=exchangename, queue=log_queue_name, routing_key='#')
+
+
+if __name__ == "__main__":  
+    connection = create_connection()
+    channel = create_channel(connection)
+    create_queues(channel)
+    
+    
---
+++ services/simple/log/src/log.py
@@ -0,0 +1,37 @@
+import amqp_connection
+import json
+import pika
+# from os import environ
+
+log_queue_name = 'All_Logs' 
+
+def receiveLog(channel):
+    try:
+        
+        channel.basic_consume(queue=log_queue_name, on_message_callback=callback, auto_ack=True)
+        print('error microservice: Consuming from queue:', log_queue_name)
+        channel.start_consuming() 
+    
+    except pika.exceptions.AMQPError as e:
+        print(f"error microservice: Failed to connect: {e}") 
+
+    except KeyboardInterrupt:
+        print("error microservice: Program interrupted by user.")
+
+
+def callback(channel, method, properties, body): 
+    print("Received Log From:" + __file__)
+    print(json.loads(body))
+    print()
+
+# define error and success logs?
+# def processSuccess(Msg): 
+# def processError(Msg):
+
+
+if __name__ == "__main__":     
+    print("error microservice: Getting Connection")
+    connection = amqp_connection.create_connection() 
+    print("error microservice: Connection established successfully")
+    channel = connection.channel()
+    receiveLog(channel)

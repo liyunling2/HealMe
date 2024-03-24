a580c2924c0692f5c976e1a4e1c851a1b8de6184
Jiale.So <jiale.so.2022@scis.smu.edu.sg>
2024-03-24 16:45:48 +0800

    Setup

---
+++ FrontEnd/healme/src/views/Tester.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about">
+      <h1>This is an tester page</h1>
+    </div>
+  </template>
+<script>
+// @ is an alias to /src
+
+export default {
+name: 'Tester',
+}
+</script>

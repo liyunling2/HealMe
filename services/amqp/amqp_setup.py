012fdf3fb5b51a12e3b2d1de6d919bdc8750fcd4
liyunling2 <yunling.li.2022@smu.edu.sg>
2024-03-28 13:55:10 +0800

    Add logging into file

--- services/amqp/amqp_setup.py
+++ services/amqp/amqp_setup.py
@@ -1,6 +1,13 @@
 import time
 import pika
 from os import environ
+import logging
+from logging.handlers import RotatingFileHandler
+
+# 1 MB max size, keep 5 backups
+handler = RotatingFileHandler(filename='../app_logs/application.log', maxBytes=1024*1024, backupCount=5) 
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logging.basicConfig(level=logging.INFO, handlers=[handler])
 
 hostname = environ.get('hostname') or "localhost"
 port = environ.get('port')  or 5672
@@ -9,48 +16,48 @@ exchangetype = "fanout"
  
 
 def create_connection(max_retries=12, retry_interval=5):
-    print('amqp_setup:create_connection')
+    logging.info('amqp_setup: create_connection')
     
     retries = 0
     connection = None
     
     while retries < max_retries:
         try:
-            print('amqp_setup: Trying connection')
+            logging.info('amqp_setup: Trying connection')
             
             connection = pika.BlockingConnection(pika.ConnectionParameters
                                 (host=hostname, port=port,
                                  heartbeat=3600, blocked_connection_timeout=3600)) 
-            print("amqp_setup: Connection established successfully")
+            logging.info("amqp_setup: Connection established successfully")
             break  
 
         except pika.exceptions.AMQPConnectionError as e:
-            print(f"amqp_setup: Failed to connect: {e}")
+            logging.error(f"amqp_setup: Failed to connect: {e}")
             retries += 1
-            print(f"amqp_setup: Retrying in {retry_interval} seconds...")
+            logging.error(f"amqp_setup: Retrying in {retry_interval} seconds...")
             time.sleep(retry_interval)
 
     if connection is None:
-        raise Exception("amqp_setup: Unable to establish a connection to RabbitMQ after multiple attempts.")
+        logging.error("amqp_setup: Unable to establish a connection to RabbitMQ after multiple attempts.")
 
     return connection
 
 def create_channel(connection):
-    print('amqp_setup:create_channel')
+    logging.info('amqp_setup:create_channel')
     channel = connection.channel()
-    print('amqp_setup:create exchange')
+    logging.info('amqp_setup:create exchange')
     channel.exchange_declare(exchange=exchangename, exchange_type=exchangetype, durable=True) 
 
     return channel
 
 
 def create_queue(channel):
-    print('amqp_setup:create queues')
+    logging.info('amqp_setup:create queues')
     create_log_queue(channel)
 
   
 def create_log_queue(channel):
-    print('amqp_setup:create_activity_log_queue')
+    logging.info('amqp_setup:create_activity_log_queue')
     log_queue_name = 'All_Logs'
     channel.queue_declare(queue=log_queue_name, durable=True) 
     channel.queue_bind(exchange=exchangename, queue=log_queue_name, routing_key='#')

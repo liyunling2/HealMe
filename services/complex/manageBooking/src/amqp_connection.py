32fedcc16054a9a75a7e1556b8996884a8fac6a6
liyunling2 <yunling.li.2022@smu.edu.sg>
2024-03-29 00:31:45 +0800

    Update rabbit details

--- services/complex/manageBooking/src/amqp_connection.py
+++ services/complex/manageBooking/src/amqp_connection.py
@@ -1,33 +1,48 @@
 import time
 import pika
 from os import environ
+import logging
+from logging.handlers import RotatingFileHandler
 
-hostname = environ.get('hostname') or "localhost"
+class LibraryLogFilter(logging.Filter):
+    def filter(self, record):
+        # Filter out log records
+        if record.name.startswith('pika.') or record.name == 'AMQP':
+            return False
+        return True
+    
+# 1 MB max size, keep 5 backups
+handler = RotatingFileHandler(filename='../../../app_logs/application.log', maxBytes=1024*1024, backupCount=5) 
+handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logging.basicConfig(level=logging.INFO, handlers=[handler])
+handler.addFilter(LibraryLogFilter())
+
+hostname = environ.get('hostname') or "rabbitmq"
 port = environ.get('port') or 5672
 
 def create_connection(max_retries=12, retry_interval=5):
-    print('amqp_connection: Create_connection')
+    logging.info('Complex Manage Booking amqp_connection: Create_connection')
     
     retries = 0
     connection = None
 
     while retries < max_retries:
         try:
-            print('amqp_connection: Trying connection')
+            logging.info('Complex Manage Booking amqp_connection: Trying connection')
             connection = pika.BlockingConnection(pika.ConnectionParameters
                                 (host=hostname, port=port,
                                  heartbeat=3600, blocked_connection_timeout=3600)) 
-            print("amqp_connection: Connection established successfully")
+            logging.info("Complex Manage Booking amqp_connection: Connection established successfully")
             break 
 
         except pika.exceptions.AMQPConnectionError as e:
-            print(f"amqp_connection: Failed to connect: {e}")
+            logging.warning(f"Complex Manage Booking amqp_connection: Failed to connect: {e}")
             retries += 1
-            print(f"amqp_connection: Retrying in {retry_interval} seconds...")
+            logging.warning(f"Complex Manage Booking amqp_connection: Retrying in {retry_interval} seconds...")
             time.sleep(retry_interval)
     
     if connection is None:
-        raise Exception("Unable to establish a connection to RabbitMQ after multiple attempts")
+        raise Exception("Complex Manage Booking unable to establish a connection to RabbitMQ after multiple attempts")
     
     return connection
 
@@ -36,7 +51,7 @@ def check_exchange(channel, exchangename, exchangetype):
         channel.exchange_declare(exchangename, exchangetype, durable=True, passive=True) 
                        
     except Exception as e:
-        print('Exception:', e)
+        logging.error('Exception:', e)
         return False
     return True
 
